111a6a6c55fad3221cb1b4cd7c5d8fb0838da27e
narudom <bapairaew@hotmail.com>
2017-03-12 08:55:31 +0700

    Tail arrow for Sidebar

--- app/components/NavItem/index.js
+++ app/components/NavItem/index.js
@@ -3,11 +3,12 @@ import styles from './nav-item.scss';
 import Icon from 'components/Icon';
 import { NavItem as _NavItem } from 'react-bootstrap';
 
-const Nav = ({ children, icon, ...props }) => (
+const Nav = ({ children, icon, tailArrow, ...props }) => (
   <_NavItem className={styles.navitem} {...props}>
     <div className={styles.container}>
       {icon && <Icon className={styles.icon} glyph={props.active ? `${icon}White` : `${icon}Purple`} />}
-      {children}
+      <div className={styles.mainContent}>{children}</div>
+      {tailArrow && <Icon width={7} height={12} glyph={props.active ? `ArrowWhite` : `ArrowPurple`} />}
     </div>
   </_NavItem>
 );
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -22,12 +22,12 @@ const App = ({ user, isSidebarOpened, openSidebar, closeSidebar }) => (
         </NavMessage>
         <NavItem icon={'Controls'} eventKey={1}>Dashboard</NavItem>
         <NavItem icon={'ShoppingCart'} eventKey={2}>Orders</NavItem>
-        <NavItem icon={'Factory'} eventKey={3}>Companies</NavItem>
+        <NavItem icon={'Factory'} tailArrow={true} eventKey={3}>Companies</NavItem>
         <NavItem icon={'Box'} eventKey={4}>Products</NavItem>
         <NavItem icon={'Document'} eventKey={5}>Documents</NavItem>
         <NavItem icon={'Hierarchy'} eventKey={6}>Pricing</NavItem>
         <NavItem icon={'Shapes'} eventKey={7}>Brands</NavItem>
-        <NavItem icon={'Gear'} eventKey={8}>Settings</NavItem>
+        <NavItem icon={'Gear'} tailArrow={true} eventKey={8}>Settings</NavItem>
         <NavItem icon={'Chart'} eventKey={9}>Reports</NavItem>
         <NavItem icon={'People'} eventKey={10}>Account Users</NavItem>
         <NavItem eventKey={11}>Log out</NavItem>
